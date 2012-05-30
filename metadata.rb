fe924478646f2082bed67e2c702f671654ecb22d
Bryan Berry <bryan.berry@gmail.com>
2012-05-30 15:26:40 +0200

    moved jboss::catalog recipe into its own cookbook

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bryan.berry@gmail.com"
 license          "Apache v2.0"
 description      "Installs/Configures jboss"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.9"
+version          "0.0.10"
 
 %w{ java logrotate }.each do |cb|
   depends cb
