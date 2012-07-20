022649daaed148aa4f26bd8594c46ac4878f6f53
Bryan Berry <bryan.berry@gmail.com>
2012-07-20 09:39:35 +0200

    add cron jobs to compress logs already rotated by jboss and delete logs older than 60 days

--- recipes/rotate_logs.rb
+++ recipes/rotate_logs.rb
@@ -7,3 +7,12 @@ logrotate_app node['jboss']['user'] do
   rotate    30
   create    "664 #{node['jboss']['user']} #{node['jboss']['user']}"
 end
+
+cron "compress rotated server.log" do
+  minute "0"
+  hour   "0"
+  command  "find /usr/local/#{node['jboss']['user']}/standalone/log -name 'server.log*.gz' -mtime +60 \
+-exec rm -f '{}' \\; \
+find /usr/local/#{node['jboss']['user']}/standalone/log -name 'server.log.20*'  -a ! -name '*.gz' -exec gzip '{}' \\;"
+end
+
